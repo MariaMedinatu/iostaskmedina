3b3359db1d4cf6d570ed38de778283890b4e3d42
mariamedinatu <maria.medina.aiesec@gmail.com>
2015-03-13 20:01:07 +0200

    ios task by Maria Medina
    ios task by Maria Medina

---
+++ iostaskmedina/iostaskmedina/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iostaskmedina
+//
+//  Created by Developer on 13/03/15.
+//  Copyright (c) 2015 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+    
